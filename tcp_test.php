a7ef2bfe8394eba41e51ad4af16004afbda700b0
wazsmwazsm <18748406022@163.com>
2017-08-02 18:32:18 +0800

    tcp test

---
+++ tcp_test.php
@@ -0,0 +1,18 @@
+<?php
+
+use Workerman\Worker;
+require_once __DIR__ . '/Workerman/Autoloader.php';
+
+// 创建一个 worker , 监听指定端口, http 协议通信
+$tcp_worker = new Worker("tcp://0.0.0.0:600");
+
+// 启动 10 个进程
+$tcp_worker->count = 10;
+
+// 响应函数
+$tcp_worker->onMessage = function($con, $data) {
+    $con->send('hello! '.$data);
+};
+
+// 启动 worker
+Worker::runAll();
