71731e586ed7cd43b1df283181791cce7a0d236b
wazsmwazsm <18748406022@163.com>
2017-08-02 18:00:21 +0800

    http_test

---
+++ http_test.php
@@ -0,0 +1,18 @@
+<?php
+
+use Workerman\Worker;
+require_once __DIR__ . '/Workerman/Autoloader.php';
+
+// 创建一个 worker , 监听指定端口, http 协议通信
+$http_worker = new Worker("http://0.0.0.0:600");
+
+// 启动 10 个进程
+$http_worker->count = 10;
+
+// 相应函数
+$http_worker->onMessage = function($con, $data) {
+    $con->send('hello! welcome to workerman!');
+};
+
+// 启动 worker
+Worker::runAll();
