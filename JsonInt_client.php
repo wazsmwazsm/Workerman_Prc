87247018156eee4fbc522b94de81ce3a58cc35c0
wazsmwazsm <18748406022@163.com>
2017-08-07 17:51:02 +0800

    fix JsonInt_client bug, can get right data now

--- JsonInt_client.php
+++ JsonInt_client.php
@@ -1,8 +1,8 @@
 <?php
 
-// 请使用 workerman 的功能实现客户端
-// 直接使用 socket 可能有粘包、拆包问题
-// 请使用 JsonInt_client2.php 来进行数据获取
+// 建议使用 workerman 的功能实现客户端
+// 直接使用 socket 可能有粘包、拆包问题 ?
+// 建议使用 JsonInt_client2.php 来进行数据获取
 // 此例子只是使用 stream socket 进行一次 tcp 访问
 
 $socket = stream_socket_client('tcp://127.0.0.1:600', $erron, $errstr);
@@ -10,16 +10,16 @@ $socket = stream_socket_client('tcp://127.0.0.1:600', $erron, $errstr);
 $socket or die($erron.$errstr);
 
 // 发送数据
-$msg = json_encode(['name'=>'jack', 'gen'=>'ma']);
-$data = pack('N', strlen($msg)).$msg;
-
+$msg = json_encode(['name'=>'jack', 'gen'=>'ma']);   // 构造包数据
+$msg_pk_length = strlen($msg) + 4;                   // 获得包长
+$data = pack('N', $msg_pk_length).$msg;              // 构成协议包
+// 发送数据
 fwrite($socket, $data);
-
+// 获取相应数据
 $response = fread($socket, 256);
-
-var_dump($response);
-
+// 关闭连接
 fclose($socket);
 $response_length = unpack('Ntotal_length', substr($response, 0, 4))['total_length'];
 $response_data = substr($response, 4);
-var_dump($response_length, $response_data);
+
+echo "返回数据包: 长度 : $response_length , 包体 : $response_data \n";
