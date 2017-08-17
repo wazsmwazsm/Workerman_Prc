3ba6a45e5447e84a0fdb43da0c0a0dc9a4735205
wazsmwazsm <18748406022@163.com>
2017-08-17 09:45:54 +0800

    TcpClass attr

--- TcpClass/attr.php
+++ TcpClass/attr.php
@@ -2,6 +2,13 @@
 
 require_once dirname(dirname(__FILE__)) . '/vendor/autoload.php';
 use Workerman\Worker;
+use Workerman\Connection\TcpConnection;
+
+// 此属性为全局静态属性，用来设置每个连接能够接收的最大包包长。不设置默认为10MB。
+TcpConnection::$maxPackageSize = 1024000;
+
+// 此属性为全局静态属性，用来设置所有连接的默认应用层发送缓冲区大小。不设置默认为1MB
+TcpConnection::$defaultMaxSendBufferSize = 2*1024*1024;
 
 $worker = new Worker('tcp://0.0.0.0:600');
 
@@ -14,9 +21,19 @@ $worker->onConnect = function($con) {
     echo 'connection id : '.$con->id." \n";
 
     // 手动设置当前连接的协议类
-    $con->protocol = 'Workerman\\Protocols\\Http';
+    $con->protocol = 'Workerman\\Protocols\\Text';
+    // 设置当前连接的应用层发送缓冲区大小,
+    // 不设置默认为Connection::$defaultMaxSendBufferSize(1MB)。
+    // Connection::$maxSendBufferSize 和 Connection::$defaultMaxSendBufferSize均可以动态设置。
+    // 此属性影响onBufferFull回调
+    $con->maxSendBufferSize = 102400;
 };
-
+$worker->onMessage = function($connection, $data) {
+    // 获取当前 connection 对象所属的 worker 实例
+    foreach ($connection->worker->connections as $con) {
+        $con->send($data);
+    }
+};
 
 // 运行worker
 Worker::runAll();
